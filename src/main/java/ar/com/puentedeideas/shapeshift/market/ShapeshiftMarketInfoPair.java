f5f632d02447728ff09a3636c1a0b42a7ee3fc7d
Fernando Wasylyszyn <ferwasy@yahoo.com.ar>
2017-06-25 22:17:53 -0300

    url in constructor

--- src/main/java/ar/com/puentedeideas/shapeshift/market/ShapeshiftMarketInfoPair.java
+++ src/main/java/ar/com/puentedeideas/shapeshift/market/ShapeshiftMarketInfoPair.java
@@ -12,18 +12,17 @@ import ar.com.puentedeideas.shapeshift.ShapeshiftResponseException;
 
 public class ShapeshiftMarketInfoPair {
 	
-	private ShapeshiftRequest shapeshiftRequest;
+	private ShapeshiftRequest request;
 	
-	private ShapeshiftPair shapeshiftPair;
+	private ShapeshiftMarketInfoPairUrl url;
 
 	public ShapeshiftMarketInfoPair(HttpClient httpClient, ShapeshiftPair pair) {
-		this.shapeshiftRequest = new ShapeshiftRequest(httpClient);
-		this.shapeshiftPair = pair;
+		this.request = new ShapeshiftRequest(httpClient);
+		this.url = new ShapeshiftMarketInfoPairUrl(pair);
 	}
 	
 	public JSONObject get() throws ShapeshiftConnectionException, ShapeshiftResponseException {
-		GetMethod getMethod = new GetMethod(new ShapeshiftMarketInfoPairUrl(this.shapeshiftPair).toString(), false);
-		return this.shapeshiftRequest.get(getMethod);
+		return this.request.get(new GetMethod(this.url.toString(), false));
 	}
 		
 }
